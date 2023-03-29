37871cd3317ca4fc3182d1a3a64d21d491b899e4
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-03-29 16:59:38 -0400

    theme modification

---
+++ src/Scenes/Global/SidBar.jsx
@@ -0,0 +1,12 @@
+
+
+const SidBar = () => {
+
+    return (
+        <div>
+            <h1> SidBar</h1>
+        </div>
+    )
+}
+
+export default SidBar
---
+++ src/Scenes/Global/TopBar.jsx
@@ -0,0 +1,11 @@
+
+const TopBar = () => {
+
+    return(
+        <div>
+            <h1>TopBar</h1>
+        </div>
+    )
+}
+
+export default TopBar
