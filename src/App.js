f3c5c149aed04a472d36cc7ddce3a33203882346
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-03-30 13:48:35 -0400

    add sidbar e top bar ao header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from "./Scenes/Theme";
+import { CssBaseline,ThemeProvider } from "@mui/material";
+import TopBar from "./Scenes/Global/TopBar";
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+function App () {
+  const [ theme, colorMode] = useMode();
+
+  return(
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+             <CssBaseline/>
+            <main>
+              <TopBar/>
+            </main>
+
+      </div>
+    </ThemeProvider>
+    
+    
+    </ColorModeContext.Provider>
+
+  )
+
+
 }
 
 export default App;
--- src/Scenes/Global/TopBar.jsx
+++ src/Scenes/Global/TopBar.jsx
@@ -1,10 +1,67 @@
+import { Box , IconButton , useTheme} from '@mui/material'
+import { useContext } from 'react'
+import { ColorModeContext, tokens } from '../Theme';
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+
+
 
 const TopBar = () => {
 
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
     return(
-        <div>
-            <h1>TopBar</h1>
-        </div>
+     <Box 
+      display= 'flex'
+      justifyContent = 'space-between'
+       p={2}
+       >
+        {/* Bar Search */}
+        <Box 
+        display= 'flex' 
+        backgroundColor={ colors.primary[400]} 
+        borderRadius='  3px'
+        >
+            <InputBase
+             sx={{ml:2, flex:1}} 
+             Placeholder='Search'           
+            />
+            <IconButton  
+            type='button'
+            sx={{p:1}}
+            >
+                <SearchIcon/>
+            </IconButton>
+        </Box>
+           { /* Icons  */}   
+           <Box display='flex'>
+            <IconButton onClick={colorMode.togleColorMode}>
+                { theme.palette.mode === 'dark' ? (
+                    <DarkModeOutlinedIcon/>
+                ) : (
+                    <LightModeOutlinedIcon/>
+                ) }
+                
+            </IconButton>
+            <IconButton>
+                <NotificationsOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                 <SettingsOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                  <PersonOutlinedIcon/>
+            </IconButton>
+           </Box>
+       </Box>
     )
 }
 
