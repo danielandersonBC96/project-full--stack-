104432ba7bb36b240115ba3722458a83521df705
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-05 13:54:23 -0400

    atualização do formulario

--- src/Scenes/Form/index.jsx
+++ src/Scenes/Form/index.jsx
@@ -1,30 +1,41 @@
 import{Box, Button , TextField } from '@mui/material'
 import { Formik} from 'formik'
-import * as yup from ' yup '
+import * as   yup from  'yup'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import Header from '../../Components/Header'
 
 
 const initialValues = {
-     firstName :'',
-     lastName:' ',
-     email:' ',
-     constact:' ',
-     address1:'',
-     address2:' '
+     firstName :'  ',
+     lastName:'  ',
+     email:'  ',
+     constact:'  ',
+     address1:'  ',
+     address2:'  ' 
 
 }
 
-const userSchema = yup.object().shape({
+const phoneRegExp = 
+    /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
 
-    
+
+
+const userSchema = yup.object().shape({
+    firstName: yup.string().required('required'),
+    lastName:yup.string().required("required"),
+    email:yup.string().required('required'),
+    constact:yup.string().required('required'),
+    address1:yup.string().required('required'),
+    address2:yup.string().required ('required')
+
 })
 
 const Form = () => {
 
-    const isNomMobile = useMediaQuery('min-width:600PX ')
+    const isNomMobile = useMediaQuery('min-width:600px ')
 
-    const handleFormSubmit = ( values) => {
+    const HandleFormSubmit = ( values) => {
+       
         console.log( values )
     }
 
@@ -32,7 +43,12 @@ const Form = () => {
         <div>
             <Box m='20px '>
                 <Header title='New User'  subtitle='Create Neu User '  />
-                <Formik>
+                <Formik
+                onSubmit={HandleFormSubmit}
+                initialValues={ initialValues}
+                validateSchema={ userSchema}
+                
+                >
 
 
                 </Formik>
