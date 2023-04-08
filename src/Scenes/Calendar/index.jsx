59d1ab5a69a45fc77cf672ef6947c3f0035c93de
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-07 22:55:27 -0400

    introduction  calendar

---
+++ src/Scenes/Calendar/index.jsx
@@ -0,0 +1,26 @@
+import { useState} from 'react'
+import FullCallendar, {formateDate } from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import  interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import{
+    Box,
+    List,
+    ListItem,
+    ListItemText,
+    Typography,
+    useTheme
+} from '@mui/material';
+import Header from '../../Components/Header';
+import { tokens } from '../Theme';
+
+const Calendar = ()  => {
+
+
+
+    
+
+}
+
+export default Calendar
