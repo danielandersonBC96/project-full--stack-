053ef05d27b716bd5159bba2dff42f3c5dc2d79b
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-09 16:11:08 -0400

    calendar part 2

--- src/Scenes/Calendar/index.jsx
+++ src/Scenes/Calendar/index.jsx
@@ -55,11 +55,42 @@ const Calendar = ()  => {
         selected.event.remove()
     }
 
- }
+ };
 
+      return(
+           <Box>
+            <Header title='CALENDAR'/>
+             <Box display='flex' justifyContent='space-between'>
+                <Box flex=' 1 1 20%' 
+                 backgroundColor={ color.primary[400]}
+                 borderRadius='4px'
+                 >
 
+                   <Typography variante ='h5'>
+                             Events
+                   </Typography>
+                   <List>
+                     { currenteEvents.map((event) => (
 
+                        <ListItem
+                        key={event.id}
+                        sx={{
+                            backgroundColor: color.greenAccen[500], 
+                            margin: '10px 0',
+                            borderRadius:' 2px',
 
+                        }}
+                        >
+                              
+
+                        </ListItem>
+                     ))}
+                   </List>
+
+                </Box>
+             </Box>
+           </Box>
+      )
 }
 
 export default Calendar
